5346c6d0ba4488a179a6a304cf53ac227ae38e49
Vasyl Zelinskiy <vasiazel96@gmail.com>
2016-12-17 16:26:07 +0200

    Added CalendarFragment. Fixed .gitignore file

--- app/src/main/java/com/flashpoint/ticktocker/CalendarFragment.java
+++ app/src/main/java/com/flashpoint/ticktocker/CalendarFragment.java
@@ -1,7 +1,58 @@
 package com.flashpoint.ticktocker;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 
 public class CalendarFragment extends Fragment {
+
+    private CalendarView calendar;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        initializeCalendar(view);
+
+        return view;
+    }
+
+    public void initializeCalendar(final View view) {
+        calendar = (CalendarView) view.findViewById(R.id.calendar);
+        // sets whether to show the week number.
+        calendar.setShowWeekNumber(false);
+        // sets the first day of week according to Calendar.
+        // here we set Monday as the first day of the Calendar
+        calendar.setFirstDayOfWeek(2);
+
+        //The background color for the selected week.
+        calendar.setSelectedWeekBackgroundColor(getResources().getColor(R.color.green));
+
+        //sets the color for the dates of an unfocused month.
+        calendar.setUnfocusedMonthDateColor(getResources().getColor(R.color.transparent));
+
+        //sets the color for the separator line between weeks.
+        calendar.setWeekSeparatorLineColor(getResources().getColor(R.color.transparent));
+
+        //sets the color for the vertical bar shown at the beginning and at the end of the selected date.
+        calendar.setSelectedDateVerticalBar(R.color.darkgreen);
+
+        //sets the listener to be notified upon selected date change.
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            //show the selected date as a toast
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
+                Toast.makeText(getContext(), day + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+            }
+
+        });
+
+    }
 }
