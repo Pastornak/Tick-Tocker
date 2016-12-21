ad9c52381b648f40da87bea432cf2960b2ebb1ab
Yurii Pasternak <yurapaster@gmail.com>
2016-12-21 23:18:43 +0200

    Cosmetic changes

--- app/src/main/java/com/flashpoint/ticktocker/ShowEventFragment.java
+++ app/src/main/java/com/flashpoint/ticktocker/ShowEventFragment.java
@@ -20,7 +20,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class ShowEventFrafment extends Fragment {
+public class ShowEventFragment extends Fragment {
 
     //private DatabaseReference database;
 
